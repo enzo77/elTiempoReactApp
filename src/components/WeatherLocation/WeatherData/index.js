cae4d6a6030e54c49349edaa86fdbfd68ee9357e
enzo santoro <enzo.Santoro@es.ddb.com>
2019-09-06 06:28:18 +0200

    Weather Folders

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import { SNOW } from './../../../constants/weather';
-
+import './styles.css';
 const WeatherData = () => (
-    <div>
+    <div className='weatherDataCont'>
         <WeatherTemperature temperature = {20} weatherState  = {SNOW} ></WeatherTemperature>
         <WeatherExtraInfo humidity = {80} wind = {'10m/s'} ></WeatherExtraInfo>
     </div>
