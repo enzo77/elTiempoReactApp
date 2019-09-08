4f242ae2c1f34a05019732c070d17ba35bfa7566
enzo77 <ngksan@gmail.com>
2019-09-08 02:55:12 +0200

    Template engines

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import { SNOW } from './../../constants/weather';
+
+const data = { 
+    temperature: 20,
+    weatherState: SNOW,
+    humidity: 10 ,
+    wind: '10 m/s',
+ }
 
 const WeatherLocation = () => (
     <div>
         <Location city = { 'Kuki'  } /> 
-        <WeatherData />
+        <WeatherData data = { data  } />
     </div>
 )
 
