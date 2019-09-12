55c5730906bf28ab8a7c6cd6366047ab1a58cb45
enzo77 <ngksan@gmail.com>
2019-09-13 01:32:22 +0200

    Estados Lesson

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
-import { SNOW } from './../../constants/weather';
+import { SNOW , WINDY } from './../../constants/weather';
 
-const data = { 
+const data1 = { 
     temperature: 20,
     weatherState: SNOW,
     humidity: 10 ,
     wind: '10 m/s',
  }
 
+ const data2 = { 
+    temperature: 10,
+    weatherState: WINDY,
+    humidity: 90 ,
+    wind: '300 m/s',
+ }
+
 class WeatherLocation extends Component { 
 
-    render = () => (
-        <div>
-            <Location city = { 'Kuki'  } /> 
-            <WeatherData data = { data  } />
-        </div>
-    )
+    constructor() { 
+        super();
+        this.state = { 
+            data : data1,
+            city: 'Maranello'
+         }
+      } 
 
-};
+    handleUpdateClick = () => { 
+        this.setState ({ 
+            data : data2,
+            city: 'Barcelona'
+        });
+     }
+
+    render = () => {
+        const { city, data } = this.state;
+        return (
+            <div>
+                <Location city = { city  } /> 
+                <WeatherData data = { data  } />
+                <button onClick={ this.handleUpdateClick }>Actualiza</button>
+            </div> )
+        }
+
+    };
 
 export default WeatherLocation;
