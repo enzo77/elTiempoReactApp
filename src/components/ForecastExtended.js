70c37d1db4c44bb3400e7241f785f23bfa2624bf
enzo77 <ngksan@gmail.com>
2020-01-15 01:38:05 +0100

    End App

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -34,14 +34,25 @@ class ForecastExtended extends Component {
     }
 
     componentDidMount(){
-        const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
+        this.updateCity(this.props.city);
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(nextProps.city !== this.props.city){
+            this.setState({ forecastData: null });
+            this.updateCity(nextProps.city);
+        }
+    }
+
+    updateCity = city => {
+        const url_forecast = `${url}?q=${city}&appid=${api_key}`;
 
         fetch(url_forecast).then(
             data => (data.json())
         ).then(
             weather_data => {
                 const forecastData = transformForecast(weather_data);
-                this.setState({forecastData  });
+                this.setState({ forecastData  });
             }
         );
     }
