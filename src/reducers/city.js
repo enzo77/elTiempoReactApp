d762c47bbad197d9113a8921a80b5ce8440d4f27
Enzo <Enzo.Santoro@es.ddb.com>
2020-03-10 10:40:41 +0100

    format

--- src/reducers/city.js
+++ src/reducers/city.js
@@ -1,12 +1,12 @@
 import { SET_CITY } from './../actions';
 
-export const city = (state , action) => {
-    switch( action.type ){
+export const city = (state, action) => {
+    switch (action.type) {
         case SET_CITY:
-            return { ...state, city : action.value };
+            return {...state, city: action.payload };
 
-            default:
-                break;
+        default:
+            break;
     }
 
     return state;
