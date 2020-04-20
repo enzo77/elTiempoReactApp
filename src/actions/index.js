b78428d7acca05762f0edfcba5f6f0281f2fe93b
Enzo <Enzo.Santoro@es.ddb.com>
2020-04-20 23:13:18 +0200

    Thunk Commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,28 @@
 export const SET_CITY = 'SET_CITY';
 
-export const setCity = payload =>({ 
+const setCity = payload =>({ 
     type: SET_CITY ,
     payload 
 });
+
+const url = "http://api.openweathermap.org/data/2.5/forecast";
+const api_key = "9baf99c72740d9bfafc2a4909e5b7a9b";
+
+export const setSelectedCity = payload =>{ 
+   return dispatch => {
+    const url_forecast = `${url}?q=${payload}&appid=${api_key}`;
+    dispatch(setCity(payload));
+
+    fetch(url_forecast).then(
+        data => (data.json())
+    ).then(
+        weather_data => {
+            const forecastData = transformForecast(weather_data);
+            this.setState({ forecastData });
+    
+        }
+    );
+    
+    return;
+   };
+};
